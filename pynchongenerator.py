a820290ac11b2e74d63064e5cd94f017daafa656
leeannmperry <38820098+leeannmperry@users.noreply.github.com>
2018-04-28 17:00:41 -0700

    Update pynchongenerator.py

--- pynchongenerator.py
+++ pynchongenerator.py
@@ -33,17 +33,6 @@ def process_textfile(filename):
     '''
     Creates a dictionary with transition pairs
     based on a file provided
-
-    For the first part of the assignment, we use a
-    placeholder text that you will have to replace
-    at some point.
-
-    Based on the placeholder text, the dictionary
-    should contain the following key-value pairs:
-
-    'blue,': ['END']
-    'by': ['yellow', 'day.', 'day?']
-    'still': ['hopping', 'going']
     '''
     d = {}
 
@@ -78,11 +67,6 @@ def generate_line(d):
 
     Note that the first state is BEGIN but that we
     obviously do not want to return BEGIN
-
-    Some sample output based on the placeholder text:
-    'i have to go to go to go to go to play,'
-
-    Hint: use random.choice to select a random element from a list
     '''
     """import random
     sentence = ""
